05bb3ec24a5054d35b135ef75160f70fca96eebd
napole-dong <phuongdong1741@gmail.com>
2025-09-04 10:33:25 +0700

    refactor(notebook): update chapter 2 notebook with ipywidgets - Refactor notebook 'Bai Tap Chuong 2/baitapchuong2.ipynb' to use ipywidgets. - Replace input-based circle area calculation with interactive widgets. - Add widgets for radius input and calculation trigger button. - Introduce widgets for second input and time conversion. - Move files from 'baitapchuong1' to 'Bai Tap Chuong 1'.

--- b1.py
+++
@@ -1,9 +0,0 @@
-import math
-try:
-    r = float(input("Mời bạn nhập bán kính hình tròn:"))
-    cv = 2 * math.pi * r
-    dt = math.pi * r ** 2
-    print("Chu vi =", cv)
-    print("Diện tích =", dt)
-except:
-    print("Lỗi rồi!")
